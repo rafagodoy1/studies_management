51c82244ab85816689db3889880647d71b3017ab
Rafa Godoy <rafagodoy547@gmail.com>
2022-04-18 15:45:21 +1000

    include reference, contribution, and study log pages

--- app.py
+++ app.py
@@ -109,3 +109,139 @@ def person_create():
             return redirect(url_for('index'))
 
     return render_template('person_create.html')
+
+@app.route('/reference-list')
+def reference_list():
+    
+    conn = get_db_connection()
+    reference = conn.execute('SELECT * FROM reference').fetchall()
+    conn.close()
+
+    return render_template('reference_list.html', reference=reference)
+
+@app.route('/reference-create', methods=('GET', 'POST'))
+def reference_create():
+    if request.method == 'POST':
+        ks_source_id = request.form['ks_source_id']
+        ks_dependant_id = request.form['ks_dependant_id']
+        is_mandatory = request.form['is_mandatory']
+        dependency_source = request.form['dependency_source']
+
+        if not ks_source_id:
+            flash('KS Source ID is required!')
+        elif not ks_dependant_id:
+            flash('KS Dependant ID is required!')
+        elif not is_mandatory:
+            flash('Is Mandatory Flag is required!')
+        elif not dependency_source:
+            flash('Dependancy Source is required!')
+        else:
+            conn = get_db_connection()
+            
+            sql_insertion = "INSERT INTO reference (ks_source_id, ks_dependant_id, is_mandatory, dependency_source) VALUES "
+            sql_insertion = sql_insertion + f"({ks_source_id}, {ks_dependant_id}, {is_mandatory}, '{dependency_source}')"
+            conn.execute(sql_insertion)
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('reference_create.html')
+
+@app.route('/interaction-list')
+def interaction_list():
+    
+    conn = get_db_connection()
+    interaction = conn.execute('SELECT * FROM interaction').fetchall()
+    conn.close()
+
+    return render_template('interaction_list.html', interaction=interaction)
+
+@app.route('/interaction-create', methods=('GET', 'POST'))
+def interaction_create():
+    if request.method == 'POST':
+        interaction_type_of_study = request.form['interaction_type_of_study']
+        ks_type = request.form['ks_type']
+        ks_id = request.form['ks_id']
+        ks_partition_type = request.form['ks_partition_type']
+        ks_partition_name = request.form['ks_partition_name']
+        ks_partition_number = request.form['ks_partition_number']
+        interaction_date = request.form['interaction_date']
+        ks_partition_metric = request.form['ks_partition_metric']
+        ks_partition_start = request.form['ks_partition_start']
+        ks_partition_finish = request.form['ks_partition_finish']
+        ks_partition_total = request.form['ks_partition_total']
+        ks_total = request.form['ks_total']
+        ks_partition_percentage = request.form['ks_partition_percentage']
+        interaction_log_type = request.form['interaction_log_type']
+        interaction_log_time_min = request.form['interaction_log_time_min']
+
+        if not ks_type:
+            flash('KS Type is required!')
+        elif not ks_id:
+            flash('KS ID is required!')
+        elif not ks_partition_type:
+            flash('Partition Type is required!')
+        elif not ks_partition_name:
+            flash('Partition Name is required!')
+        elif not ks_partition_number:
+            flash('Partition NUmber is required!')
+        elif not interaction_date:
+            flash('Study Date is required!')
+        elif not ks_partition_metric:
+            flash('Partition Metric is required!')
+        elif not ks_partition_start:
+            flash('Partition Start is required!')
+        elif not ks_partition_finish:
+            flash('Partition Finish is required!')
+        elif not ks_partition_total:
+            flash('Partition Total is required!')
+        elif not ks_total:
+            flash('KS Total is required!')
+        elif not ks_partition_percentage:
+            flash('Partition Percentage is required!')
+        elif not interaction_log_type:
+            flash('Log Type is required!')
+        elif not interaction_log_time_min:
+            flash('Log Time is required!')
+        else:
+            conn = get_db_connection()
+
+            sql_insertion = f"""INSERT INTO interaction (
+                interaction_type_of_study,
+                ks_type,
+                ks_id,
+                ks_partition_type,
+                ks_partition_name,
+                ks_partition_number,
+                interaction_date,
+                ks_partition_metric,
+                ks_partition_start,
+                ks_partition_finish,
+                ks_partition_total,
+                ks_total,
+                ks_partition_percentage,
+                interaction_log_type,
+                interaction_log_time_min
+            ) VALUES (
+                '{interaction_type_of_study}',
+                '{ks_type}',
+                {ks_id},
+                '{ks_partition_type}',
+                '{ks_partition_name}',
+                '{ks_partition_number}',
+                '{interaction_date}',
+                '{ks_partition_metric}',
+                '{ks_partition_start}',
+                '{ks_partition_finish}',
+                {ks_partition_total},
+                {ks_total},
+                {ks_partition_percentage},
+                '{interaction_log_type}',
+                {interaction_log_time_min}
+            );"""
+            conn.execute(sql_insertion)
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('interaction_create.html')
--- schema.sql
+++ schema.sql
@@ -18,6 +18,8 @@ CREATE TABLE person (
 	person_name TEXT NOT NULL
 );
 
+DROP TABLE IF EXISTS reference;
+
 CREATE TABLE reference (
 	reference_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	reference_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -25,4 +27,36 @@ CREATE TABLE reference (
 	ks_dependant_id INTEGER NOT NULL,
 	is_mandatory BOOLEAN NOT NULL,
 	dependency_source TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS contribution;
+
+CREATE TABLE contribution (
+	contribution_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	contribution_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	person_id INTEGER NOT NULL,
+	ks_id INTEGER NOT NULL,
+	contribution_type TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS interaction;
+
+CREATE TABLE interaction (
+	interaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	interaction_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	interaction_type_of_study TEXT NOT NULL,
+	ks_type TEXT NOT NULL,
+	ks_id INTEGER NOT NULL,
+	ks_partition_type TEXT NOT NULL,
+	ks_partition_name TEXT NOT NULL,
+	ks_partition_number TEXT NOT NULL,
+	interaction_date TIMESTAMP NOT NULL,
+	ks_partition_metric TEXT NOT NULL,
+	ks_partition_start TEXT NOT NULL,
+	ks_partition_finish TEXT NOT NULL,
+	ks_partition_total INTEGER NOT NULL,
+	ks_total INTEGER NOT NULL,
+	ks_partition_percentage NUMBER NOT NULL,
+	interaction_log_type TEXT NOT NULL,
+	interaction_log_time_min INTEGER NOT NULL
 );
